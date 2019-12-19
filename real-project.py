fc6b33e1290ed389c8ad17f11b2e3bc0e35edff6
rzimbalist <rzimbalist@ucvts.org>
2019-12-19 18:47:57 -0500

    made entire plan out of comments

---
+++ real-project.py
@@ -0,0 +1,9 @@
+# plan:
+# - make a maze where the user's mouse is tracked
+# - use pixels to draw the maze with multiple traps, but not too
+#   difficult resulting in the user failing from trying to take normal path
+# - make the user lose and restart if a wall (filled pixel) is touched
+# - make multiple levels of the maze, and allow each to have
+#   a level of difficulty from easy to hard
+# - make a win/lose message
+# - possibly add a timed gamemode
